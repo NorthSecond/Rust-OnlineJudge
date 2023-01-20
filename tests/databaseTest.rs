fb95126f76e279318712d95807af1cf1db6dc1d3
Whisper <wy288170679@163.com>
2023-01-20 12:13:34 +0800

    添加插入数据语法

--- tests/databaseTest.rs
+++ tests/databaseTest.rs
@@ -4,12 +4,12 @@ mod tests {
     use mysql::prelude::*;
     use chrono::prelude::*; // 用来处理日期
     
+    //查询数据
     #[test]
-    fn connect() {
+    fn query() {
         let url = "mysql://RUST-OJ:123456@localhost:3306/rustoj";
         let pool = Pool::new(url).unwrap(); // 获取连接池
         let mut conn = pool.get_conn().unwrap();// 获取链接
-
 
         conn.query_iter("select * from `tb_user`")
             .unwrap()
@@ -18,4 +18,22 @@ mod tests {
                 println!("{}, {}, {}, {}", r.0, r.1, r.2, r.3);
             });
     }
+
+    //插入数据
+    #[test]
+    fn insert() {
+        let url = "mysql://RUST-OJ:123456@localhost:3306/rustoj";
+        let pool = Pool::new(url).unwrap(); // 获取连接池
+        let mut conn = pool.get_conn().unwrap();// 获取链接
+
+        conn.exec_drop("Insert into `tb_user`(`user_name`, `user_password`, `email`) values (:name,:password,:email);", 
+        params! {
+            "name" => "test",
+            "password" => "test",
+            "email" => "test",
+        }).unwrap();
+
+        println!("Insert successfully!");
+    }
+
 }
